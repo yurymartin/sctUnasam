ba39dad781137eb1c3b3568cc2ceae6183f5d0ea
martinyuri123 <yuri_1030@hotmail.com>
2020-06-02 10:44:09 -0500

    add

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -5,10 +5,10 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>SCT-19  | UNASAM</title>
+    <title>SCT-19 | UNASAM</title>
     <!-- Font Awesome -->
     <link rel="stylesheet" href="../../plugins/fontawesome-free/css/all.min.css">
-    <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('dist/css/adminlte.min.css') }}">
     <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 </head>
 
@@ -17,9 +17,9 @@
         <app></app>
     </div>
     <script src="{{ mix('js/app.js') }}"></script>
-    <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
-    <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-    <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
+    <script src="{{ secure_asset('plugins/jquery/jquery.min.js') }}"></script>
+    <script src="{{ secure_asset('plugins/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ secure_asset('dist/js/adminlte.min.js') }}"></script>
 </body>
 
 </html>
